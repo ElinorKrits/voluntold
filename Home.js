40ec7d6feafa85a10e8411cd8e469ae164f5a959
ElinorKrits <54546749+ElinorKrits@users.noreply.github.com>
2021-11-01 14:43:47 -0500

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,12 @@
+import './styles.css';
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div id="header">
+            VOLUNTOLD
+        </div>
+    );
+};
+
+export default Home;
